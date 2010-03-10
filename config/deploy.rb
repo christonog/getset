371cebe946e5ab051da2a5068c5c365586c5f69e
Chris Tonog <christonog@christopher-tonogs-macbook.local>
2010-03-10 00:30:38 -0500

    added symlink automation for database.yml file

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,14 @@ role :web, ip
 role :app, ip
 role :db, ip, :primary => true
 
-set :deploy_to, "/home/#{user}/#{applicationdir}" 
+set :deploy_to, "/home/#{user}/#{applicationdir}"
+set :shared_dir, "#{deploy_to}/shared"
+set :release_path, "/#{deploy_to}/current"
 set :group_writable, false
+
+desc "Link in the production database.yml" 
+task :after_update_code do
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
+end
+
 
