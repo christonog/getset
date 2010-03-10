52f48adc70944f5cfe09489af581db3b1e2b75dd
Chris Tonog <christonog@christopher-tonogs-macbook.local>
2010-03-10 01:17:30 -0500

    made more changes to deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :group_writable, false
 
 desc "Link in the production database.yml" 
 task :after_update_code do
-  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml" 
+  run "ln -nfs #{release_path}/config/database.yml #{deploy_to}/#{shared_dir}/config/database.yml" 
 end
 
 
