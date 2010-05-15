02bdf7239fa27c059576303301f7673e95808342
Chris Tonog <chris@getsetapp.com>
2010-05-15 01:57:46 -0400

    added some changes to the view copy text

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -2,8 +2,8 @@ class LocationController < ApplicationController
   before_filter :set_title, :set_meta_description
   
   def set_meta_description
-     @meta_description = "Calculate your driving travel costs with Getset. Enter your travel destination, your current location, gas price,
-      and your car's gas mileage. Give Getset a try! It's so easy to find out how much it costs to drive to your destination. You can change the
+     @meta_description = "Calculate your travel costs with Getset. Enter your travel destination, your current location, gas price,
+      and your car's gas mileage. Give Getset a try! It's so easy to find out how much it costs to travel to your destination. You can change the
       gas price and gas mileage to get a more accurate calculation of your driving costs."
   end
   
@@ -11,12 +11,12 @@ class LocationController < ApplicationController
   end
   
   def start
-     @title = "Getset | Find out your driving costs" 
+     @title = "Getset | Find out your travel costs" 
   end
 
   def results
     @location = Location.new(params[:location])
-    @title = "Driving cost from #{@location.city_from} to #{@location.city_to}"  
+    @title = "Flight and driving cost from #{@location.city_from} to #{@location.city_to}"  
   end
   
   def about
