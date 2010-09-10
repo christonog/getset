f2a99495e1f8053d7f60731cd1eccbd02270618c
Chris Tonog <chris@getsetapp.com>
2010-09-10 02:12:45 -0400

    fixed some issues in view and deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :group_writable, false
 
 desc "Link in the production database.yml" 
 task :after_update_code do
+  run "rm -f #{release_path}/config/database.yml"
   run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
 end
 
