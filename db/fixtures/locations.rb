568671271634435aaeb8300df36af5b3ce089b79
Chris Tonog <chris@getsetapp.com>
2010-09-11 01:21:44 -0400

    removed contents of fixtures/locations.rb

--- db/fixtures/locations.rb
+++
@@ -1,6 +0,0 @@
-Location.seed_many(:city_to, :city_from, [
-  { :city_to => "Raleigh, NC", :city_from => "New York - JFK" },
-  { :city_to => "Los Angeles, CA", :city_from => "Miami, FL" },
-  { :city_to => "New York, NY", :city_from => "Raleigh, NC" }
-  
-])
