a39c6d0d6b9204cd29a5e24c73adcdded382e677
Chris Tonog <chris@getsetapp.com>
2010-09-12 16:05:04 -0400

    edited deploy file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ task :symlink_shared do
   run out
 end
 
-after 'deploy:after_update_code', 'deploy:symlink_shared'
+after 'deploy:update_code', 'deploy:symlink_shared'
 
 =begin
 desc "Link in the production database.yml" 
