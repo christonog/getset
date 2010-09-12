1618397ae686f5e10741e0052757c6340e18f85b
Chris Tonog <chris@getsetapp.com>
2010-09-12 11:47:31 -0400

    as per oleg's request, changed config/deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,10 +17,20 @@ role :db, ip, :primary => true
 set :deploy_to, "/home/#{user}/#{applicationdir}"
 set :group_writable, false
 
+desc 'Symlink shared configs and folders on each release.'
+task :symlink_shared do
+  out =  [ 'public/system', 'config/database.yml', 'config/mongrel_cluster.yml', 'vendor/rails' ].collect do |name|
+           "ln -s #{shared_path}/#{name} #{release_path}/#{name}"
+         end.join(' && ')
+  run out
+end
+
+=begin
 desc "Link in the production database.yml" 
 task :after_update_code, :roles => [:web, :db, :app] do
   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
 end
+=end
 
 desc "Link Wordpress" 
 task :after_update_code do
