5539fcf5f3490d7ae957de1fff45123586ae4e2a
Chris Tonog <chris@getsetapp.com>
2010-09-12 20:52:55 -0400

    made changes to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,79 @@
 set :user, 'getsetap'
+
+set :scm, :git
+
+set :branch, :master
+
+set :ip, '68.233.9.46'
+
+set :application, 'getset'
+
+set :applicationdir, 'getset' 
+
+set :repository, "git@getset.sourcerepo.com:getset/getset.git" 
+
+set :use_sudo, false
+
+set :keep_releases, 5
+
+
+
+default_run_options[:pty] = true
+
+
+
+role :web, ip
+
+role :app, ip
+
+role :db, ip, :primary => true
+
+
+
+set :deploy_to, "/home/#{user}/#{applicationdir}"
+
+set :group_writable, false
+
+
+
+after 'deploy:update_code', 'deploy:symlink_shared'
+
+after 'deploy:update_code', 'deploy:symlink_wordpress'
+
+
+
+namespace :deploy do
+
+  desc 'Symlink shared configs and folders on each release.'
+
+  task :symlink_shared do
+
+    out =  [ 'public/system', 'config/database.yml', 'vendor/rails' ].collect do |name|
+
+             "ln -s #{shared_path}/#{name} #{release_path}/#{name}"
+
+           end.join(' && ')
+
+    run out
+
+  end
+
+
+
+  desc "Link Wordpress" 
+
+  task :symlink_wordpress do
+
+    run "ln -s #{shared_path}/blog #{release_path}/public" 
+
+  end
+
+end
+
+
+=begin
+
+set :user, 'getsetap'
 set :scm, :git
 set :branch, :master
 set :ip, '68.233.9.46'
@@ -32,10 +107,10 @@ desc "Link in the production database.yml"
 task :after_update_code, :roles => [:web, :db, :app] do
   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
 end
-=end
 
 desc "Link Wordpress" 
 task :after_update_code do
   run "ln -s #{shared_path}/blog #{release_path}/public" 
 end
 
+=end
